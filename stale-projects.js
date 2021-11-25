b4b7639353ed962c91a89c4d641ddd1e861c502e
André Wendt <awendt@babbel.com>
2021-11-25 14:01:39 +0100

    removeStaleProjects removes projects sequentially

--- stale-projects.js
+++ stale-projects.js
@@ -2,8 +2,10 @@ exports.getStaleProjects = (currentProjects, targetProjects) => (
   currentProjects.filter((p) => !targetProjects.includes(p.node.project.name))
 );
 
-exports.removeStaleProjects = async ({ client, projects }) => (
-  Promise.all(projects.map((staleProject) => (
-    client.rest.projects.deleteCard({ card_id: staleProject.node.databaseId })
-  )))
-);
+exports.removeStaleProjects = async ({ client, projects }) => {
+  // process projects sequentially to reduce risk of throttling by GitHub API
+  for (const project of projects) {
+    // eslint-disable-next-line no-await-in-loop
+    await client.rest.projects.deleteCard({ card_id: project.node.databaseId });
+  }
+};
